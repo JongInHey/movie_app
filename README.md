a406865863ace9f4fa897a5fd77926575627a052
JongInHey <youyi1234@naver.com>
2024-07-15 16:42:09 +0900

    api setup end

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - [x] Router 설정
 - [x] 각 컴포넌트, 폴더 등 설정
-- [] Api 설정
+- [x] Api 설정
 - [] Header, Footer 컴포넌트 구성
 - [] Loading 컴포넌트 구성
 - [] 웹 폰트 설정
@@ -13,7 +13,7 @@
 
 ## 설치 항목
 
-- [] react-router-dom
+- [x] react-router-dom
 - [] styled-components
 - [] styled-reset
 - [] swiper
@@ -22,4 +22,3 @@
 - [] helmet-async
 - [] react-icons
 - []
-
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
-import { nowPlaying } from "../../api";
+import { nowPlaying, popular, topRated, upcoming } from "../../api";
 
 export const Home = () => {
   const [nowData, setNowData] = useState();
+  const [popData, setPopData] = useState();
+  const [topData, setTopData] = useState();
+  const [upData, setUpData] = useState();
 
   useEffect(() => {
     (async () => {
       try {
-        const { results } = await nowPlaying();
-        setNowData(results);
+        const { results: nowResult } = await nowPlaying();
+        const { results: popResult } = await popular();
+        const { results: topResult } = await topRated();
+        const { results: upResult } = await upcoming();
+        setNowData(nowResult);
+        setPopData(popResult);
+        setTopData(topResult);
+        setUpData(upResult);
       } catch (error) {
         console.log(error);
       }
@@ -16,6 +25,9 @@ export const Home = () => {
   }, []);
 
   console.log(nowData);
+  // console.log(`인기 영화 :  ${popData}`);
+  // console.log(`평점 좋음 :  ${topData}`);
+  // console.log(`개봉 예정작 : ${upData}`);
 
   return <div>Home</div>;
 };
