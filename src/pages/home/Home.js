9069bb8148c839f1c84706d8252c69598aa1958e
JongInHey <youyi1234@naver.com>
2024-07-16 17:34:44 +0900

    mobile w768 setting

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -20,11 +20,26 @@ const MainBanner = styled.section`
   }
 
   p {
-    width: 600px;
+    max-width: 500px;
+    width: 100%;
     font-size: 20px;
     line-height: 30px;
     opacity: 0.7;
     font-weight: 300;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 550px ${spacing.moSide} 0 ${spacing.moSide};
+
+    h3 {
+      font-size: 40px;
+      margin-bottom: 15px;
+    }
+    p {
+      width: 100%;
+      font-size: 14px;
+      line-height: 20px;
+    }
   }
 `;
 
